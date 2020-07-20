fc4c88fbdb8b13c64dbefd2f453a4df0d8a3ea8c
Tamas Panyi <panyitamas@gmail.com>
2020-07-20 22:23:32 +0200

    MessageQueue defined in .h

--- include/MessageQueue.h
+++ include/MessageQueue.h
@@ -1,13 +1,38 @@
 #ifndef MESSAGE_QUEUE_H
 #define MESSAGE_QUEUE_H
 
-template <class T>
+#include <iostream>
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+
+/*
+Template functions, including member functions, must be written entirely in the header file!
+*/
+
+template <class T>  
 class MessageQueue {
   public:
-    T receive();
-    void send(T &&message);
+    T receive(){
+      std::unique_lock<std::mutex> lck(mtx);
+      _cond.wait(lck, [this](){return !_queue.empty();});
+      T msg = std::move(_queue.back());
+      _queue.pop_back();
+    return msg;
+    };
+
+    void send(T &&message){
+      std::lock_guard<std::mutex> lck(mtx);
+      this->_queue.emplace_back(std::move(message));
+      _cond.notify_one();
+    };
+    //TODO: remove in prod
+    void test() {std::cout<<"Test from Message queue \n";};
   private:
-    int _count;
+    // TODO: internal counter? int _count;
+    std::mutex mtx;
+    std::deque<T> _queue;
+    std::condition_variable _cond;
 };
 
 #endif
