e8122834366cfe1d0f266714f2da2a540500d602
Tamas Panyi <panyitamas@gmail.com>
2020-07-24 22:45:24 +0200

    Tiles shows - firs iteration

--- src/Tiles.cpp
+++ src/Tiles.cpp
@@ -1,9 +1,9 @@
 #include "Tiles.h"
 #include "Renderer.h"
+#include <SDL.h>
 
 Tiles::Tiles(int id){
-  tilesMatrix = std::vector<std::vector<int>>(3, std::vector<int>(3));
-  
+  tilesMatrix.resize( 10, std::vector<int>( 3 , 1 ) );
 }
 
 void Tiles::Update(){
@@ -11,6 +11,21 @@ void Tiles::Update(){
 }
 
 void Tiles::Draw(Renderer &renderer){
+  int height = 10;
+  int width = 10;
+  for (int i = 0; i<10; i++){
+    for (int j = 0; j<5; j++){
+      SDL_Rect rect; 
+      rect.x = i*60+40;
+      rect.y = j*30+20;
+      rect.w = 40;
+      rect.h = 20;
+      SDL_SetRenderDrawColor(renderer.sdl_renderer, 10, 12, 0, 0xFF);
+      SDL_RenderFillRect(renderer.sdl_renderer, &rect);
+    }
+  }
+    
+ 
 
 }
 
