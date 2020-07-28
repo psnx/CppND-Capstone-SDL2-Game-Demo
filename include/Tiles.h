a9283b46308800ad6f84de53832de8379f233a64
Tamas Panyi <panyitamas@gmail.com>
2020-07-28 22:51:02 +0200

    fixed performance hit - matrix in init

--- include/Tiles.h
+++ include/Tiles.h
@@ -2,6 +2,7 @@
 #define TILES_H
 
 #include <vector>
+#include <SDL.h>
 #include "GameObject.h"
 
 class Tiles : public GameObject {
@@ -15,6 +16,8 @@ class Tiles : public GameObject {
     std::vector<std::vector<int>> tilesMatrix;
     int rows{3};
     int columns{10};
+    std::vector<SDL_Rect> tilesList;
+    
 };
 
 #endif
--- src/Tiles.cpp
+++ src/Tiles.cpp
@@ -3,8 +3,14 @@
 #include <SDL.h>
 
 Tiles::Tiles(int id){
-  tilesMatrix.resize( columns, std::vector<int>( rows , 1 ) );
-  tilesMatrix[2][2] = 0;
+  int columns{10};
+  int rows{10};
+  int height = 10;
+  int width = 10;
+  for (int i = 0; i<columns * rows; i++){
+    SDL_Rect t = {i/rows*60+40, i%rows *30+20, 40, 20};
+    tilesList.emplace_back(std::move(t));
+  }
 }
 
 void Tiles::Update(){
@@ -12,24 +18,10 @@ void Tiles::Update(){
 }
 
 void Tiles::Draw(Renderer &renderer){
-  int height = 10;
-  int width = 10;
-  for (int i = 0; i<columns; i++){
-    for (int j = 0; j<rows; j++){
-      SDL_Rect rect; 
-      rect.x = i*60+40;
-      rect.y = j*30+20;
-      rect.w = 40;
-      rect.h = 20;
-      if (tilesMatrix[i][j]== 1){
-        SDL_SetRenderDrawColor(renderer.sdl_renderer, 10, 12, 0, 0xFF);
-        SDL_RenderFillRect(renderer.sdl_renderer, &rect);
-      }
-    }
-  }
-    
- 
-
+  for (const SDL_Rect &rect : tilesList ){
+    SDL_SetRenderDrawColor(renderer.sdl_renderer, 10, 12, 0, 0xFF);
+    SDL_RenderFillRect(renderer.sdl_renderer, &rect);
+  } 
 }
 
 Tiles::~Tiles(){
